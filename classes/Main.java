3cd44ad183427b55066f286e1286bfcd182f6ff9
Umut Berkay Ünal <57957472+umutberkayunal@users.noreply.github.com>
2022-10-20 10:59:58 +0300

    Value and Reference Types Exercise
    This program includes class, arrays and primitive types and their memory manipulation in order to understand value and reference types.

--- classes/Main.java
+++ classes/Main.java
@@ -3,6 +3,8 @@ package classes;
 public class Main {
 
 	public static void main(String[] args) {
+		//reference type
+		//reference types create a pointer on the stack and points to the heap. all the information is stored on heap
 		CustomerManager customerManager = new CustomerManager();
 		System.out.println(customerManager.number);
 		CustomerManager customerManager2 = customerManager;
@@ -24,6 +26,20 @@ public class Main {
 		customerManager.add();
 		customerManager.remove();
 		customerManager.update();
+		
+		//value-type
+		//value-types are on the stack.
+		int number1 = 10;
+		int number2 = 20;
+		number2 = number1;
+		number1 = 30;
+		System.out.println(number2);
+		
+		int[] numbers1 = new int[] {1, 2, 3};
+		int[] numbers2 = new int[] {4, 5, 6};
+		numbers2 = numbers1;
+		numbers1[0] = 50;
+		System.out.println(numbers2[0]);
 	}
 
 }
