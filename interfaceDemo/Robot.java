8dcacda1f3954c2e3d399eafc2866ae9e63d505c
Umut Berkay Ünal <57957472+umutberkayunal@users.noreply.github.com>
2022-10-26 13:20:16 +0300

    Multiple interface implementation exercise
    This program includes multiple interface implementations on different classes to learn multiple - interface implementation.

---
+++ interfaceDemo/Robot.java
@@ -0,0 +1,11 @@
+package interfaceDemo;
+
+public class Robot implements IWorkable{//doesn't eat, isn't paid. May be maintainable.
+
+	@Override
+	public void work() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
